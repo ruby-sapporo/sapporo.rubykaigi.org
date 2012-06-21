30c4eb3fe5b532707e23a1ed6d10ed08863ba1e1
Yoji SHIDARA <dara@shidara.net>
2012-06-21 20:42:50 +0900

    Be more informative about the source location

--- 2012/_team/team_renderer.rb
+++ 2012/_team/team_renderer.rb
@@ -3,6 +3,7 @@
 
 require 'yaml'
 require 'erb'
+require 'pathname'
 
 def i18n(value, lang='en')
   value[lang] || value['en']
@@ -11,19 +12,25 @@ end
 def render_web(locale)
   @locale = locale
   @title = {'ja' => '実行委員会', 'en' => 'Organizing Team'}
-  @team = YAML.load_file(File.join(File.dirname(__FILE__), 'team.yml'))
 
-  template_path = File.join(
-    File.dirname(__FILE__),
-    './templates/team.html.erb'
-  )
+  script_path = Pathname.new(__FILE__)
+  basedir = script_path.dirname
+  yaml_path = basedir + 'team.yml'
+  template_path = basedir + './templates/team.html.erb'
+  dest_path = basedir + ('../../_includes/2012/%s/team.html' % locale)
+
+  @team = YAML.load_file(yaml_path)
+
+  @path_to_script = script_path.expand_path.relative_path_from(
+    dest_path.expand_path.dirname
+  ).to_path
+  @path_to_yaml = yaml_path.expand_path.relative_path_from(
+    dest_path.expand_path.dirname
+  ).to_path
+
   erb_template = File.read(template_path)
   rendered_html = ERB.new(erb_template, nil, '-').result(binding)
 
-  dest_path = File.join(
-    File.dirname(__FILE__),
-    '../../_includes/2012/%s/team.html' % locale
-  )
   File.write(dest_path, rendered_html)
 end
 
