cd3a39399f3d5d5929f977d26c267ee661ca185d
Ryunosuke SATO <tricknotes.rs@gmail.com>
2012-06-21 18:27:04 +0900

    Modify team title

--- 2012/_team/team_renderer.rb
+++ 2012/_team/team_renderer.rb
@@ -10,7 +10,7 @@ end
 
 def render_web(locale)
   @locale = locale
-  @title = {'ja' => '実行委員会', 'en' => 'Team'}
+  @title = {'ja' => '実行委員会', 'en' => 'Organizing Team'}
   @team = YAML.load_file(File.join(File.dirname(__FILE__), 'team.yml'))
   open(File.join(File.dirname(__FILE__), '../../_includes/2012/%s/team.html' % locale), 'w') do |f|
     f << ERB.new(File.read('./templates/team.html.erb')).result(binding)
