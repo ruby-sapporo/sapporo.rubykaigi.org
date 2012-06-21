d07c553c633ba69f79d10110e729ad6b37f5a50f
Yoji SHIDARA <dara@shidara.net>
2012-06-21 20:06:57 +0900

    Cleanup renderer

--- 2012/_team/team_renderer.rb
+++ 2012/_team/team_renderer.rb
@@ -12,9 +12,19 @@ def render_web(locale)
   @locale = locale
   @title = {'ja' => '実行委員会', 'en' => 'Organizing Team'}
   @team = YAML.load_file(File.join(File.dirname(__FILE__), 'team.yml'))
-  open(File.join(File.dirname(__FILE__), '../../_includes/2012/%s/team.html' % locale), 'w') do |f|
-    f << ERB.new(File.read(File.join(File.dirname(__FILE__), './templates/team.html.erb')), nil, '-').result(binding)
-  end
+
+  template_path = File.join(
+    File.dirname(__FILE__),
+    './templates/team.html.erb'
+  )
+  erb_template = File.read(template_path)
+  rendered_html = ERB.new(erb_template, nil, '-').result(binding)
+
+  dest_path = File.join(
+    File.dirname(__FILE__),
+    '../../_includes/2012/%s/team.html' % locale
+  )
+  File.write(dest_path, rendered_html)
 end
 
 render_web('ja')
